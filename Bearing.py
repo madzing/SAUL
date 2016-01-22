14e4d5d27b4b7e4a50393c4905f60c8ae2678a42
Madzing <janmetzing@gmx.de>
2016-01-22 20:53:45 +0100

    calculateMeanBearing funktioniert

--- Bearing.py
+++ Bearing.py
@@ -15,11 +15,11 @@ class Bearing():
             else:
                 return -(self.getDiffTo360degree(self.bearing)-self.getDiffTo360degree(bearing))
 
-    def getDiffTo360degree(self,B):
-        if B.bearing<=180:
-            return B.bearing
+    def getDiffTo360degree(self,bearing):
+        if bearing.bearing<=180:
+            return bearing.bearing
         else:
-            return 360 - B.bearing
+            return 360 - bearing.bearing
 
     def add(self,bearing):
         if type(bearing) == "Bearing":
@@ -44,5 +44,31 @@ class Bearing():
             return Bearing(360 + diff)
         else:
             return Bearing(0-diff)
-    def calculateMeanBearing(self,BearingList):
-        pass
+
+    @staticmethod
+    def calculateMeanBearing(bearingList):
+        x = 0
+        y = 0
+        for element in bearingList:
+            if element.bearing <= 90:
+                x = x + math.cos(math.radians(90 - element.bearing))
+                y = y + math.sin(math.radians(90 - element.bearing))
+            elif element.bearing <= 180:
+                x = x + math.sin(math.radians(180 - element.bearing))
+                y = y - math.cos(math.radians(180 - element.bearing))
+            elif element.bearing <= 270:
+                x = x - math.cos(math.radians(270-element.bearing))
+                y = y - math.sin(math.radians(270-element.bearing))
+            else:
+                x = x - math.sin(math.radians(360-element.bearing))
+                y = y + math.cos(math.radians(360-element.bearing))
+        if x > 0 and y >= 0:
+            return 90 - math.degrees(math.atan(y/x))
+        elif x >= 0 and y < 0:
+            return 180 - math.degrees(math.atan(x/math.fabs(y)))
+        elif x < 0 and y <= 0:
+            return 270 - math.degrees(math.atan(math.fabs(y)/math.fabs(x)))
+        elif x <= 0 and y > 0:
+            return 360 - math.degrees(math.atan(math.fabs(x)/y))
+        else:
+            return None
