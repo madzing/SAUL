8b465cd84ad92b0c0483a0b2728794b0a0865419
esperan23 <da.heitzig@gmail.com>
2016-02-12 19:47:08 +0100

    Testcase gefixt

--- testSailor.py
+++ testSailor.py
@@ -76,7 +76,7 @@ sens = Sensors()
 sailor = Sailor(sens)
 
 print ("\nTest5: Erwartungshaltung: Schiff soll weiter nach links (Backbord) fahren!")
-desiredBearing = Bearing(270)
+desiredBearing = Bearing(330)
 sens.setWindDirection(0)
 sens.setCompassBearing(30)
 sens.setWinkelgesch(0)
